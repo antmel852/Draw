ca3cf0eab3c2515f6c6e1d738f30c2bd2276efcd
anton <melnikov1994@gmail.com>
2022-03-29 23:51:38 +0300

    add cors

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const PORT = process.env.PORT || 5000;
 const router = require('./router');
 
 const app = express();
+app.use(cors());
+
 app.use(express.static(path.resolve(__dirname, "./build")));
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '/build', 'index.html'));
@@ -22,7 +24,6 @@ corsOptions={
 }
 const io = socketio(server, corsOptions);
 
-app.use(cors());
 
 
 // New connection
