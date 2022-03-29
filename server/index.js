b0b90d17c88cf8148317415166c4dda72cc11c9b
anton <melnikov1994@gmail.com>
2022-03-29 20:39:01 +0300

    add path

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 const cors = require('cors');
+const path = require('path');
 
 const { addUser, removeUser, getUser, getUsersInRoom} = require('./users.js');
 
